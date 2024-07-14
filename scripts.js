d9d578a6fd0fdd7050a3ff93b6995cf60fbd2d30
zxycode <152382705+zxycode7@users.noreply.github.com>
2024-07-14 23:14:17 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
-// JavaScript代码可以在这里添加，例如处理导航菜单的交互等
-console.log("Welcome to ZXYCode7 Blog!");
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 
 // 导航菜单鼠标悬停效果
 document.querySelectorAll('nav a').forEach(link => {
